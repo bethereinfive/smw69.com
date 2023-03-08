c3dbddb3f7bc583a7232962805265f529834f8d8
Softlabltd.com <freelancernishad123@gmail.com>
2023-03-08 17:06:15 +0600

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>VNSHOPIE Earn Money</title>
+    <title>welcome to smw</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/img/logos.png') }}">
