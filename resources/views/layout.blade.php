79fe1b44d5d7b6e267237d28a5cbab3755f87837
Softlabltd.com <freelancernishad123@gmail.com>
2023-03-08 17:07:25 +0600

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <title>welcome to smw</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/img/logos.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('header.jpeg') }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- <!-- Favicon -->
